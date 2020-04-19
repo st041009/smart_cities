7a6bc75da39698573f8acc2c50a63b06f6983e0b
st041009 <st041009@student.spbu.ru>
2020-04-19 22:53:20 +0300

    second sommit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world</p>
+        <p>SPheat test reactJS</p>
       </header>
     </div>
   );
