5cea04a822a15449f2178dd7c5c54e3fc1c86953
st041009 <st041009@student.spbu.ru>
2020-04-20 12:11:59 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>SPheat test reactJS</p>
+        <p>Second check</p>
       </header>
     </div>
   );
